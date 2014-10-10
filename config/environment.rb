54078354c35d0af185fb00930f0680b3cb9b4307
Samantha Drago <sfdrago@Samanthas-MacBook-Air-2.local>
2014-10-10 16:12:01 -0700

    stuff

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+# config.i18n.enforce_available_locales = true
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
